5b1b1533063c466e42143072777d8615ee624699
ecthelion5109 <xanderseling@gmail.com>
2023-07-16 01:28:12 -0300

    add

---
+++ documents & misc/Modding_EcthTests/Comapring archers vs lings.py
@@ -0,0 +1,40 @@
+spiderling_health = 100
+takes_pierce = 0.73 #% ;;.;; 69% ;;-;; 65% ;;=;;  100%   ; 100%; ; 100 ; ; 110%
+
+lorienarcher = 40
+deals = .95
+
+
+# print("109v3 b61",lorienarcher*0.75*1)
+# print("109v3 b60",lorienarcher*deals*takes_pierce)
+# print("106",40*1*1)
+
+# print("GA 109v3 b60",30*1.25*0.73)
+# print("GA 109v3 b61",30*1.15*0.80)
+
+
+
+
+
+print("gondorarcher 109v1    ",30*1.0*0.65)
+print("gondorarcher 109v2    ",30*1.25*0.69)
+print("gondorarcher 109v3 b60",30*1.25*0.73)
+print("gondorarcher 109v3 b61",30*1.15*0.80)
+
+
+
+print("ranger 106    ",65*1.0*1)
+print("ranger 109v1    ",65*1.0*0.65)
+print("ranger 109v2    ",65*1.35*0.69)
+print("ranger 109v3 b60",65*1.21*0.73)
+print("ranger 109v3 b61",65*1.10*0.80)
+
+
+
+
+print("lorienbow 106    ",40*1.00*1)
+print("lorienbow 109v1    ",40*0.90*0.65)
+print("lorienbow 109v2    ",40*0.95*0.69)
+print("lorienbow 109v3 b60",40*0.95*0.73)
+print("lorienbow 109v3 b61",40*1.00*0.80)
+# print("ranger 109v3 b60",65*1.21*takes_pierce)
