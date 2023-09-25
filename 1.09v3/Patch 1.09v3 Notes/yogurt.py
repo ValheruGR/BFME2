e346c5eb68ced153f390b9b322ce78e1bf294945
ecthelion5109 <xanderseling@gmail.com>
2023-09-25 07:04:51 -0300

    -md generator

--- 1.09v3/Patch 1.09v3 Notes/yogurt.py
+++ 1.09v3/Patch 1.09v3 Notes/yogurt.py
@@ -12,13 +12,13 @@ class LogEntry:
 		self.beta = self.segments[0].replace("-", "")
 		self.faction = self.segments[1]
 		self.predicado = self.segments[3]
-		self.object = LogEntry.ObjectClass(self.segments[2])
+		self.objects = LogEntry.ObjectClass(self.segments[2])
 		
 		self.__validate_length()
 		self.__validate_faction()
 		
 	def __str__(self):
-		return f"Beta: {self.beta}\n\tFaction: {self.faction}\n\tObjectType: {self.object} \n\tPredicado: {self.predicado}\n\tObjectLen: {self.__length}"
+		return f"Beta: {self.beta}\n\tFaction: {self.faction}\n\tObjectType: {self.objects.full} \n\tPredicado: {self.predicado}\n\tObjectLen: {self.__length}"
 		
 	def __validate_length(self):
 		if self.__length != 4:
@@ -39,7 +39,7 @@ class LogEntry:
 				
 		def __validate(self):
 			if self.type not in LogEntry.ObjectClass.OBJECT_TYPE:
-				raise Exception(f"This object type is not defined: {self.type}")
+				raise Exception(f"This type of this object is not defined: {self.type}")
 				
 		def __str__(self):
 			return self.type
@@ -63,28 +63,46 @@ class Changelog:
 	def __len__(self):
 		return len(self.instances)
 		
-	def filter(beta=None, faction=None, object=None):
+	def filter(beta=None, faction=None, objects=None):
 		# print(Beta)
 		pass
+		
+	def __write_log(self, log_name, log_content):
+		log_name = f"{log_name}.md"
+		with open(log_name, 'w') as pretty_log:
+			pretty_log.write(log_content)
 	
-	
-	
+	def get_beta_log(self, beta_n, write=False):
+		log_name = f"Beta{beta_n}"
+		log_content = f"# {log_name}"
+		for query in self.instances:
+			if query.beta == log_name:
+				log_content += f"\n\n## {query.faction}"
+				log_content += f"\n\n### {query.objects.full}"
+				log_content += f"\n\n- {query.predicado}"
+		print(log_content)
+		if write:
+			self.__write_log(log_name, log_content)
+			
+		
 changelog_path = Path.cwd() / "Patch 1.09v3_Changes from 1.09v2.ini"
 log = Changelog(changelog_path)
 
-for a in log.instances:
+log.get_beta_log("60",write=True)
+	
+# for a in log.instances:
 	# self.beta
 	# self.faction
 	# self.predicado
-	# self.object
-	# self.object.type
-	# self.object.full
+	# self.objects
+	# self.objects.type
+	# self.objects.full
 		
 		
-	# if a.faction in "Elves" and a.object == "SpellBook":
-	# if a.faction in "Elves" and a.object == "SpellBook":
-	if a.beta == "Beta40":
-		print(a.object.full)
-		# print(a.object.title)
+	# if a.faction in "Elves" and a.objects == "SpellBook":
+	# if a.faction in "Elves" and a.objects == "SpellBook":
+	# if a.beta == "Beta40":
+		# print(a)
+		# print(a.objects.title)
 
  
