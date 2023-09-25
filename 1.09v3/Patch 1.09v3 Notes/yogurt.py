ecaee2e988b3e281f15ee8cec0242b3259193469
ecthelion5109 <xanderseling@gmail.com>
2023-09-25 06:32:08 -0300

    -Filter and class nesting.

--- 1.09v3/Patch 1.09v3 Notes/yogurt.py
+++ 1.09v3/Patch 1.09v3 Notes/yogurt.py
@@ -1,105 +1,90 @@
 from pathlib import Path
 
 
-changelog_path = Path.cwd() / "Patch 1.09v3_Changes from 1.09v2.ini"
 
 class LogEntry:
+	FACTIONS = {"MenOfTheWest", "Elves", "Dwarves", "Isengard", "Mordor", "Goblins", "Global", "Neutral", "Creeps"}
+	
 	def __init__(self, line):
-		colon_splited_line = line.split(":")
-		self.length = len(colon_splited_line)
+		self.segments = list(map(lambda s: s.strip(), line.split(":")))
 		
-		self.beta = colon_splited_line[0].strip().replace("-", "")
-		self.faction = colon_splited_line[1].strip()
-		self.object_type = None
-		self.change_type = None
-		self.type3 = None
-		self.type4 = None
-		if self.length >3:
-			self.object_type = colon_splited_line[2].strip()
-		if self.length >4:
-			self.change_type = colon_splited_line[3].strip()
-		if self.length >5:
-			self.type3 = colon_splited_line[4].strip()
-		if self.length >6:
-			self.type4 = colon_splited_line[5].strip()
-			
-		self.remains = colon_splited_line[-1].strip()
-		# self.remains = ".".join(colon_splited_line[2:]).strip()
+		self.__length = len(self.segments)
+		self.beta = self.segments[0].replace("-", "")
+		self.faction = self.segments[1]
+		self.predicado = self.segments[3]
+		self.object = LogEntry.ObjectClass(self.segments[2])
 		
+		self.__validate_length()
+		self.__validate_faction()
 		
-	
 	def __str__(self):
-		return f"Beta: {self.beta}\n\tFaction: {self.faction}\n\tType: {self.object_type} \n\tType2: {self.change_type} \n\tType3: {self.type3}\n\tType4: {self.type4}\n\tRemains: {self.remains}\n\tObjectLen: {self.length}"
+		return f"Beta: {self.beta}\n\tFaction: {self.faction}\n\tObjectType: {self.object} \n\tPredicado: {self.predicado}\n\tObjectLen: {self.__length}"
 		
-lista = []
-
-FACTIONS = {"MenOfTheWest", "Elves", "Dwarves", "Isengard", "Mordor", "Goblins", "Global", "Neutral", "Creeps"}
-OBJECT_TYPES = {"Structures", "Structures.Walls", "Structures.Wells", "Heroes", "Upgrades", "Units", "SpellBook", "Maps", "Misc", "ForMappers", "Artillery", "Monsters"}
-
-with open(changelog_path, 'r') as changelog:
-	for line in changelog:
-		line = line.strip()
-		if line.startswith("-Beta"):
-			entry = LogEntry(line)
-			lista.append(entry)
-			# print(line)
+	def __validate_length(self):
+		if self.__length != 4:
+			raise Exception(f"Parser error: The following segments are not 4: \n {self.segments}")
 			
-			
-			
-			
-			
-		
-def verificar_lengths():
-	for entry_length in range(7):
-		contador = 0
-		for logentry in lista:
-			if logentry.length == entry_length:
-				contador += 1
-		print("entry_length", entry_length, contador)	
-			
-def check_mymum(entry_length):
-	contador = 0
-	for logentry in lista:
-		# if logentry.faction == "Elves":
-		# pass
-			 
-		if logentry.length == entry_length:
-			contador += 1
-			print(logentry)
-		# if True:
-		# if logentry.change_type not in CHANGE_TYPE and logentry.change_type is not None:
-			# if True:
-			# if logentry.change_type is not None and logentry.change_type not in CHANGE_TYPE:
-				# print(logentry.change_type)
-				# print("\t\t", logentry.remains)
-				# pass
-				# print(logentry.faction)
-				# print(logentry.object_type)
-			# if True:
-				# print(logentry)
-				# print("\n\n\n\n")
-				# if logentry.length > 3:
-	print("entry_length", entry_length, contador)	
-	# print(len(list(filter(lambda x: x.length==7, lista))))		
+	def __validate_faction(self):
+		if self.faction not in LogEntry.FACTIONS:
+			raise Exception(f"This faction is not defined: {self.faction}")
 	
-verificar_lengths()
-# check_mymum(5)
-# entry_length 0 0
-# entry_length 1 0
-# entry_length 2 0
-# entry_length 3 0
-# entry_length 4 1160
-# entry_length 5 110
-# entry_length 6 0
-# entry_length 6 0
-			
+	
+	class ObjectClass:
+		OBJECT_TYPE = {"Structures", "Heroes", "Upgrades", "Units", "Ships", "SpellBook", "Maps", "Misc", "ForMappers", "Artillery", "Monsters"}
+		
+		def __init__(self, text):
+			self.full = list(map(lambda s: s.strip(), text.split(".")))
+			self.type = self.full[0]
+			self.__validate()
+				
+		def __validate(self):
+			if self.type not in LogEntry.ObjectClass.OBJECT_TYPE:
+				raise Exception(f"This object type is not defined: {self.type}")
+				
+		def __str__(self):
+			return self.type
+		def __eq__(self, compare):
+			return self.type == compare
 
+class Changelog:
+	
+	def __init__(self, changelog_path):
+		self.instances = []
+		
+		with open(changelog_path, 'r') as raw_log:
+			for line in raw_log:
+				line = line.strip()
+				if line.startswith("-Beta"):
+					entry = LogEntry(line)
+					self.instances.append(entry)
+		print(f"Changelog parsered: {len(self)} log entries were created ")
+					
+		
+	def __len__(self):
+		return len(self.instances)
+		
+	def filter(beta=None, faction=None, object=None):
+		# print(Beta)
+		pass
+	
+	
+	
+changelog_path = Path.cwd() / "Patch 1.09v3_Changes from 1.09v2.ini"
+log = Changelog(changelog_path)
 
-	# for count, logentry in enumerate(lista, start=1):
-		# if logentry.faction == "Elves":
-			# print(count, logentry)
+for a in log.instances:
+	# self.beta
+	# self.faction
+	# self.predicado
+	# self.object
+	# self.object.type
+	# self.object.full
+		
+		
+	# if a.faction in "Elves" and a.object == "SpellBook":
+	# if a.faction in "Elves" and a.object == "SpellBook":
+	if a.beta == "Beta40":
+		print(a.object.full)
+		# print(a.object.title)
 
-
-
-
-
+ 
